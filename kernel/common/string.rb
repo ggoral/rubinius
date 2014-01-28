0f70778835cc2e58b264651f4ffe1dc582396c1e
Yorick Peterse <yorickpeterse@gmail.com>
2014-01-28 22:48:15 +0100

    Basic implementation of String#scrub.

--- kernel/common/string.rb
+++ kernel/common/string.rb
@@ -1909,6 +1909,46 @@ class String
     result
   end
 
+  # Removes invalid byte sequences from a String, available since Ruby 2.1.
+  def scrub(replace = nil)
+    new_str = ''
+
+    # The default replacement character is the "Unicode replacement" character.
+    # (U+FFFD).
+    if !replace and !block_given?
+      replace = "\xEF\xBF\xBD".force_encoding("UTF-8")
+        .encode(self.encoding, :undef => :replace, :replace => '?')
+    end
+
+    if replace and !replace.is_a?(String)
+      raise(
+        TypeError,
+        "no implicit conversion of #{replace.class} into String"
+      )
+    end
+
+    if replace and !replace.valid_encoding?
+      raise(
+        ArgumentError,
+        "replacement must be a valid byte sequence '#{replace.inspect}'"
+      )
+    end
+
+    chars.each do |char|
+      if char.valid_encoding?
+        new_str << char
+      else
+        if block_given?
+          new_str << yield(char)
+        else
+          new_str << replace
+        end
+      end
+    end
+
+    return new_str
+  end
+
   def []=(index, count_or_replacement, replacement=undefined)
     if undefined.equal?(replacement)
       replacement = count_or_replacement
